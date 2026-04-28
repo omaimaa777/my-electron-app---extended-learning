d450cd5aaec5d27cb5c79ba318fe3d7980e116d1
Omaimaa Nageeb <182335521+omaimaa777@users.noreply.github.com>
2026-04-28 13:27:01 +0200

    trying to remove the scroll bar

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { app, BrowserWindow } = require("electron");
 function createWindow() {
   const win = new BrowserWindow({
     width: 214,
-    height: 228,
+    height: 240,
     resizable: false,
     maximizable: false,
     fullscreenable: false,
